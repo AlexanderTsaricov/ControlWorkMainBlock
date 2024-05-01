edaec374db269bb75a07424681f58d8c3b3ff0a1
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-05-01 03:02:45 +0200

    add program

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+string[] GetStriingArray()
+{
+    Console.WriteLine("Введите слова массива через запятую");
+    string enterWords = Console.ReadLine();
+    string[] enterArray = enterWords.Split(',');
+    return enterArray;
+}
+
+void PrintStringArray(string[] arr)
+{
+    foreach (string str in arr)
+    {
+        Console.Write($"{str} ");
+    }
+    Console.WriteLine();
+}
+
+string[] GetStringWordLess3Symbol(string[] enterArr)
+{
+    int lengthEndArr = 0;
+    for (int i = 0; i < enterArr.Length; i++)
+    {
+        if (enterArr[i].Length <= 3)
+        {
+            lengthEndArr++;
+        }
+    }
+    string[] endArray = new string[lengthEndArr];
+    for (int i = 0, j = 0; i < enterArr.Length; i++)
+    {
+        if (enterArr[i].Length <= 3)
+        {
+            endArray[j] = enterArr[i];
+            j++;
+        }
+    }
+    return endArray;
+}
+
+string[] enterArray = GetStriingArray();
+string[] endArray = GetStringWordLess3Symbol(enterArray);
+PrintStringArray(endArray);
